035a2b5247492571c2fe6ea72e5dce8a03f709b3
yoann cabrol <cabrolyoann@hotmail.fr>
2023-10-16 11:47:08 +0200

    test3

--- src/main/java/tennis/Main.java
+++ src/main/java/tennis/Main.java
@@ -2,8 +2,8 @@ package tennis;
 
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.printf("Hello World");
+    public static void main(String[] args){
+
     }
 }
 
